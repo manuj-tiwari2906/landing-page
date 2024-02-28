2e146069209f87081ec5d9fa8e083c5f479aac90
manujt <manuka72tiwari@gmail.com>
2024-02-28 18:36:01 +0530

    changes

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Sidebar from "../../img/sidebar.png";
 import Ecommerce from "../../img/ecommerce.png";
 import HOC from "../../img/hoc.png";
 import MusicApp from "../../img/musicapp.png";
 import { themeContext } from "../../Context";
-import OnlineClass from '../../img/online-maximum.jpeg'
-import OfflineClass from '../../img/classroom-maximum.jpg'
-
+import OnlineClass from "../../img/online-maximum.jpeg";
+import OfflineClass from "../../img/classroom-maximum.jpg";
 
 const Portfolio = () => {
   const theme = useContext(themeContext);
@@ -17,8 +16,10 @@ const Portfolio = () => {
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>JOIN OUR PROGRAM</span>
-      <span>STAY AHEAD OF THE LEAGUE</span>
+      <div className="heading-container">
+        <span style={{ color: darkMode ? "white" : "" }}>JOIN OUR PROGRAM</span>
+        <span>STAY AHEAD OF THE LEAGUE</span>
+      </div>
 
       {/* slider */}
       <Swiper
@@ -31,17 +32,39 @@ const Portfolio = () => {
           <div className="swipe-card">
             <img src={OnlineClass} />
             <div className="card-head">Online Training Courses</div>
-            <div style={{ textAlign: 'left' }}>Career defining courses. Practical Approach. Global Certifications</div>
-            <div style={{ color: '#fca61f', fontWeight: 600, width: '100%', textAlign: 'left' }}>{'View Programs ->'}</div>
+            <div style={{ textAlign: "left" }}>
+              Career defining courses. Practical Approach. Global Certifications
+            </div>
+            <div
+              style={{
+                color: "#fca61f",
+                fontWeight: 600,
+                width: "100%",
+                textAlign: "left",
+              }}
+            >
+              {"View Programs ->"}
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="swipe-card">
-          <img src={OfflineClass} />
-          <div className="card-head">Classroom Training Program</div>
-          <div  style={{ textAlign: 'left' }}>Career defining courses. Practical Approach. Global Certifications</div>
-          <div style={{ color: '#fca61f', fontWeight: 600, width: '100%', textAlign: 'left' }}>{'View Programs ->'}</div>
-        </div>
+          <div className="swipe-card">
+            <img src={OfflineClass} />
+            <div className="card-head">Classroom Training Program</div>
+            <div style={{ textAlign: "left" }}>
+              Career defining courses. Practical Approach. Global Certifications
+            </div>
+            <div
+              style={{
+                color: "#fca61f",
+                fontWeight: 600,
+                width: "100%",
+                textAlign: "left",
+              }}
+            >
+              {"View Programs ->"}
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
